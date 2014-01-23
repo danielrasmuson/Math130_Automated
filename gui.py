9c626c378073e1c49c79927ca2bcc66ec4a44824
Auengun <gregory.dosh@mnsu.edu>
2014-01-22 18:52:28 -0600

    Slowly organizing code & grouping things together for readability

--- gui.py
+++ gui.py
@@ -9,29 +9,8 @@ class Frame(wx.Frame):
     def __init__(self):
         wx.Frame.__init__(self, None,title="Math 130 Automated Grading System", pos=(50,50), size=(800,600), style =wx.DEFAULT_FRAME_STYLE)
         self.SetMinSize((800,600))
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
 
-        # Utility stuff in order to get a menu
-        # and a status bar in the bottom for the future.
-        self.menuBar = wx.MenuBar()
-
-        self.filemenu = wx.Menu()
-        self.m_open = self.filemenu.Append(wx.OPEN, "&Open Document Directory\tAlt-O", "Select the directory containing the student documents.")
-        self.m_load = self.filemenu.Append(wx.ID_ANY, "&Load Question Bank\tAlt-L", "Load's a new question bank for grading purposes.")
-        self.m_exit = self.filemenu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
-        self.Bind(wx.EVT_MENU, self.OnOpen, self.m_open)
-        self.Bind(wx.EVT_MENU, self.LoadBank, self.m_load)
-        self.Bind(wx.EVT_MENU, self.OnClose, self.m_exit)
-        self.menuBar.Append(self.filemenu, "&File")
-
-        self.helpmenu = wx.Menu()
-        self.m_about = self.helpmenu.Append(wx.ID_ABOUT, "&About", "Information about this program")
-        self.Bind(wx.EVT_MENU, self.OnAbout, self.m_about)
-        self.menuBar.Append(self.helpmenu, "&Help")
-
-        self.SetMenuBar(self.menuBar)
-
-        self.statusbar = self.CreateStatusBar()
+        self.buildMenuNav()
 
         # We need a panel in order to put stuff on
         # and then we are adding the things we want to see on this panel.
@@ -59,7 +38,7 @@ class Frame(wx.Frame):
 
         # This is our lab tree list and also the label above it.
         self.lab_tree_list = wx.TreeCtrl(self.mainpanel, 1, size=wx.Size(200,-1),style=wx.TR_HAS_BUTTONS|wx.TR_HIDE_ROOT|wx.TR_LINES_AT_ROOT)
-        self.lab_tree_list.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnSelChanged, id=1)
+        self.lab_tree_list.Bind(wx.EVT_TREE_SEL_CHANGED, self.onSelChanged, id=1)
         self.lab_tree_label = wx.StaticText(self.mainpanel, wx.ID_ANY, 'Student List')
         self.tree_list_sizer.Add(self.lab_tree_label,0,wx.ALIGN_CENTER)
         self.tree_list_sizer.Add(self.lab_tree_list, 1,wx.GROW)
@@ -93,31 +72,104 @@ class Frame(wx.Frame):
         self.right_sizer.Add(si_sizer, proportion=0, flag=wx.ALL|wx.EXPAND, border=5)
         self.right_sizer.Add(wx.StaticLine(self.mainpanel, wx.ID_ANY), 0, wx.LEFT|wx.RIGHT|wx.EXPAND, 5)
         
-        
-        # These contain all of our buttons along the bottom of the app.
-        self.b_prev = wx.Button(self.mainpanel, wx.ID_ANY, "Previous")
-        self.b_prev.SetToolTipString("Selects the previous student of the current section.")
-        self.b_prev.Bind(wx.EVT_BUTTON, self.PreviousButton)
-        self.bottom_button_sizer.Add(self.b_prev, 0,wx.ALL,5)
-
-        self.b_next = wx.Button(self.mainpanel, wx.ID_ANY, "Next")
-        self.b_next.SetToolTipString("Selects the next student of the current section")
-        self.b_next.Bind(wx.EVT_BUTTON, self.NextButton)
-        self.bottom_button_sizer.Add(self.b_next, 0,wx.ALL,5)
-
-        self.bottom_button_sizer.Add(wx.BoxSizer(wx.HORIZONTAL), 1, wx.GROW, 0)
-
-        # I was a little confused by open so I added Document
-        self.b_open = wx.Button(self.mainpanel, wx.ID_ANY, "Open Document") #works great
-        self.b_open.SetToolTipString("Opens the Document Inspector (for now).")
-        self.b_open.Bind(wx.EVT_BUTTON, self.openDocument)
-        self.bottom_button_sizer.Add(self.b_open, 0,wx.ALL,5)
-
         # This last code just finally sets the main sizer
         # on the main box and calls the layout routine.
+        self.buildBottomNav(self.mainpanel,self.bottom_button_sizer)
+        
         self.mainpanel.SetSizer(self.main_sizer)
         self.mainpanel.Layout()
+        
+    def buildMenuNav(self):
+        ''' Builds the menu bar, status bar, and the associated
+        functions required for these things to work including loading
+        questions and also selecting folders/directories.'''
+        def onClose(event):
+            dlg = wx.MessageDialog(self,
+                "Do you really want to close this application?",
+                "Confirm Exit", wx.OK|wx.CANCEL|wx.ICON_QUESTION)
+            result = dlg.ShowModal()
+            dlg.Destroy()
+            if result == wx.ID_OK:
+                self.Destroy()
+                
+        def loadBank(event):
+            dlg = wx.FileDialog(self, "Choose a lab file:",defaultFile="lab1.dat",defaultDir=os.getcwd(), style=wx.FD_OPEN)
+            dlg.SetWildcard("Lab Dictionaries (*.dat)|*.dat")
+            if dlg.ShowModal() == wx.ID_OK:
+                self.qb.load(dlg.GetPath())
+                try:
+                    self.questions_area.Destroy()
+                except:
+                    pass
+                self.initializeQuestionArea()
+            dlg.Destroy()
 
+        def onOpen(event):
+            # I get the current working directory + the examples test stuff
+            # so that while testing it'll default to our test directory.
+            # Later we can navigate anywhere we want and cut that part.
+            dlg = wx.DirDialog(self, "Choose a directory:",os.getcwd()+"\\Examples\\test")
+            if dlg.ShowModal() == wx.ID_OK:
+                self.assignmentStack = getAssignmentStack(dlg.GetPath())
+                # Call our initial tree list build
+                self.updateTreeList(self.lab_tree_list)
+            dlg.Destroy()
+            self.lab_tree_list.SelectItem(self.lab_tree_list.GetFirstVisibleItem())
+
+        def onAbout(event):
+            dlg = wx.MessageDialog(self, "Written by Daniel Rasmuson and Gregory Dosh", "About", wx.OK)
+            result = dlg.ShowModal()
+            dlg.Destroy()
+
+        self.Bind(wx.EVT_CLOSE, onClose)
+        # Utility stuff in order to get a menu
+        # and a status bar in the bottom for the future.
+        menuBar = wx.MenuBar()
+
+        fileMenu = wx.Menu()
+        m_open = fileMenu.Append(wx.OPEN, "&Open Document Directory\tAlt-O", "Select the directory containing the student documents.")
+        m_load = fileMenu.Append(wx.ID_ANY, "&Load Question Bank\tAlt-L", "Load's a new question bank for grading purposes.")
+        m_exit = fileMenu.Append(wx.ID_EXIT, "E&xit\tAlt-X", "Close window and exit program.")
+        self.Bind(wx.EVT_MENU, onOpen, m_open)
+        self.Bind(wx.EVT_MENU, loadBank, m_load)
+        self.Bind(wx.EVT_MENU, onClose, m_exit)
+        menuBar.Append(fileMenu, "&File")
+
+        helpMenu = wx.Menu()
+        m_about = helpMenu.Append(wx.ID_ABOUT, "&About", "Information about this program")
+        self.Bind(wx.EVT_MENU, onAbout, m_about)
+        menuBar.Append(helpMenu, "&Help")
+
+        self.SetMenuBar(menuBar)
+        
+        self.statusbar = self.CreateStatusBar()
+
+    def buildBottomNav(self, panel, sizer):
+        '''Builds a predefined set of buttons on a specific panel
+         and utilizing the sizer provided'''
+        def openDocument(event):
+            current_item = self.lab_tree_list.GetItemText(self.lab_tree_list.GetSelection())
+            if "Section" not in current_item:
+                print "start \""+self.assignmentStack[current_item].getStudentFilepath()+"\""
+                os.system("\""+self.assignmentStack[current_item].getStudentFilepath()+"\"")
+        b_prev = wx.Button(panel, wx.ID_ANY, "Previous")
+        b_prev.SetToolTipString("Selects the previous student of the current section.")
+        b_prev.Bind(wx.EVT_BUTTON, self.previousButton)
+        sizer.Add(b_prev, 0,wx.ALL,5)
+
+        b_next = wx.Button(panel, wx.ID_ANY, "Next")
+        b_next.SetToolTipString("Selects the next student of the current section")
+        b_next.Bind(wx.EVT_BUTTON, self.nextButton)
+        sizer.Add(b_next, 0,wx.ALL,5)
+
+        sizer.Add(wx.BoxSizer(wx.HORIZONTAL), 1, wx.GROW, 0)
+
+        # I was a little confused by open so I added Document
+        b_open = wx.Button(panel, wx.ID_ANY, "Open Document") #works great
+        b_open.SetToolTipString("Opens the Document Inspector (for now).")
+        b_open.Bind(wx.EVT_BUTTON, openDocument)
+        sizer.Add(b_open, 0,wx.ALL,5)
+        
     def updateTreeList(self, tree):
         """Tree List on Left Side - Dynamic to Files"""
         for name in self.assignmentStack.keys():
@@ -129,30 +181,24 @@ class Frame(wx.Frame):
                 self.tree_rootDict[sec] = tree.AppendItem(self.tree_root, "Section "+sec)
             tree.AppendItem(self.tree_rootDict[sec], name) #appends name onto section
 
-    def OnSelChanged(self, event):
+    def onSelChanged(self, event):
         # Get our item that updated
         item = event.GetItem()
         if "Section" not in self.lab_tree_list.GetItemText(item):
             name = self.lab_tree_list.GetItemText(item)
             section = self.assignmentStack[name].getSection()
             # # @TODO get tech id
-            self.UpdateStudentInformation(name, section, "Unknown")
+            self.updateStudentInformation(name, section, "Unknown")
             if self.initialized:
-                self.UpdateQuestions(name)
+                self.updateQuestions(name)
 
-    def UpdateStudentInformation(self, name, section, techid):
+    def updateStudentInformation(self, name, section, techid):
         self.si_name.SetValue(name)
         self.si_section.SetValue(section)
         self.si_right.SetValue("")
         self.si_wrong.SetValue("")
 
-    def openDocument(self, event):
-        current_item = self.lab_tree_list.GetItemText(self.lab_tree_list.GetSelection())
-        if "Section" not in current_item:
-            print "start \""+self.assignmentStack[current_item].getStudentFilepath()+"\""
-            os.system("\""+self.assignmentStack[current_item].getStudentFilepath()+"\"")
-        
-    def PreviousButton(self, event):
+    def previousButton(self, event):
         current = self.lab_tree_list.GetSelection()
         prev = self.lab_tree_list.GetPrevSibling(current)
         if prev.IsOk() and not self.lab_tree_list.ItemHasChildren(prev):
@@ -164,7 +210,7 @@ class Frame(wx.Frame):
             if parent != self.lab_tree_list.GetRootItem():
                 self.lab_tree_list.SelectItem(parent)
 
-    def NextButton(self, event):
+    def nextButton(self, event):
         current = self.lab_tree_list.GetSelection()
         if self.lab_tree_list.ItemHasChildren(current):
             next = self.lab_tree_list.GetFirstChild(current)[0]
@@ -176,29 +222,8 @@ class Frame(wx.Frame):
             parent = self.lab_tree_list.GetItemParent(self.lab_tree_list.GetSelection())
             if self.lab_tree_list.GetNextSibling(parent).IsOk():
                 self.lab_tree_list.SelectItem(self.lab_tree_list.GetNextSibling(parent))
-
-    def OnClose(self, event):
-        dlg = wx.MessageDialog(self,
-            "Do you really want to close this application?",
-            "Confirm Exit", wx.OK|wx.CANCEL|wx.ICON_QUESTION)
-        result = dlg.ShowModal()
-        dlg.Destroy()
-        if result == wx.ID_OK:
-            self.Destroy()
-
-    def LoadBank(self, event):
-        dlg = wx.FileDialog(self, "Choose a lab file:",defaultFile="lab1.dat",defaultDir=os.getcwd(), style=wx.FD_OPEN)
-        dlg.SetWildcard("Lab Dictionaries (*.dat)|*.dat")
-        if dlg.ShowModal() == wx.ID_OK:
-            self.qb.load(dlg.GetPath())
-            try:
-                self.questions_area.Destroy()
-            except:
-                pass
-            self.InitializeQuestionArea()
-        dlg.Destroy()
-
-    def InitializeQuestionArea(self):
+                
+    def initializeQuestionArea(self):
         self.questions_area = wx.ScrolledWindow(self.mainpanel)
         self.questions_area.SetScrollbars(1, 1, 650, 1000)
         self.questions_area.EnableScrolling(True,True)
@@ -229,7 +254,7 @@ class Frame(wx.Frame):
         self.initialized = True
         self.mainpanel.Layout()
 
-    def UpdateQuestions(self, name):
+    def updateQuestions(self, name):
         # This gets our students answers and the dictionary we're comparing their answer to.
         # Yep I was going to tell you to pass in the name variable. That speeds things up
         studentQD = self.assignmentStack[name].getStudentDictionary()
@@ -248,23 +273,6 @@ class Frame(wx.Frame):
                 right += 1
         self.si_right.SetValue(str(right))
         self.si_wrong.SetValue(str(wrong))
-
-    def OnOpen(self, event):
-        # I get the current working directory + the examples test stuff
-        # so that while testing it'll default to our test directory.
-        # Later we can navigate anywhere we want and cut that part.
-        dlg = wx.DirDialog(self, "Choose a directory:",os.getcwd()+"\\Examples\\test")
-        if dlg.ShowModal() == wx.ID_OK:
-            self.assignmentStack = getAssignmentStack(dlg.GetPath())
-            # Call our initial tree list build
-            self.updateTreeList(self.lab_tree_list)
-        dlg.Destroy()
-        self.lab_tree_list.SelectItem(self.lab_tree_list.GetFirstVisibleItem())
-
-    def OnAbout(self, event):
-        dlg = wx.MessageDialog(self, "Written by Daniel Rasmuson and Gregory Dosh", "About", wx.OK)
-        result = dlg.ShowModal()
-        dlg.Destroy()
 
 if __name__ == "__main__":
     # Error messages go to pop-up window
